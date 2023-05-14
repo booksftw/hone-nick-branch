67945d4b069601c6399d2a067b991897644fd710
Nicholas Zachary <39927058+booksftw@users.noreply.github.com>
2023-05-13 17:23:38 -0700

    setup formik, yup, material ui, and tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@mui/material"
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const SignupSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    lastName: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+  });
 
   return (
     <>
@@ -28,6 +43,42 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <h1 className="text-3xl font-bold underline">
+        Tailwind Test
+      </h1>
+
+      <Button variant="contained">Material UI test</Button>
+
+      <h1 className='text-3xl'>Formik and Yup Test</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        validationSchema={SignupSchema}
+        onSubmit={values => {
+          // same shape as initial values
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Field name="firstName" />
+            {errors.firstName && touched.firstName ? (
+              <div>{errors.firstName}</div>
+            ) : null}
+            <Field name="lastName" />
+            {errors.lastName && touched.lastName ? (
+              <div>{errors.lastName}</div>
+            ) : null}
+            <Field name="email" type="email" />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
     </>
   )
 }
